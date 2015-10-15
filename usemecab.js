ae5a452f20ba22a87151468c209044e027428058
discord4415 <raiden000discord@gmail.com>
2015-10-16 07:15:35 +0900

    split ,

--- usemecab.js
+++ usemecab.js
@@ -24,6 +24,9 @@ check = function(){
 
 onebyone = function(){
 	console.log('start onebyone');	fs.readFileSync('./test_result','utf8').toString().split('\n').forEach(function(line){
+		line.split(',').forEach(function(line){
+			console.log(line);
+		});
 		console.log(line);
 	});
 };
