87575af1faebea49ba2c84e3a1539f703eabf23c
discord4415 <raiden000discord@gmail.com>
2015-10-25 16:19:26 +0900

    seiki

--- usemecab.js
+++ usemecab.js
@@ -24,8 +24,8 @@ check = function(){
 
 onebyone = function(){
 	console.log('start onebyone');	fs.readFileSync('./test_result','utf8').toString().split('\n').forEach(function(line){
-		line.split(',').forEach(function(line){
-			line.split('　').forEach(function(line){
+		line.split('^\s+').forEach(function(line){
+			line.split(',').forEach(function(line){
 				console.log(line);
 			});
 		});
