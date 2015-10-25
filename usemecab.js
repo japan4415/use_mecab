26613cb763231107a0d804e733547712fd039146
discord4415 <raiden000discord@gmail.com>
2015-10-25 16:42:37 +0900

    , henkan

--- usemecab.js
+++ usemecab.js
@@ -24,7 +24,8 @@ check = function(){
 
 onebyone = function(){
 	console.log('start onebyone');	fs.readFileSync('./test_result','utf8').toString().split('\n').forEach(function(line){
-		line.split('\t+'|',').forEach(function(line){
+		line.replace(',','<>');
+		line.split('<>').forEach(function(line){
 				console.log(line);
 		});
 	});
