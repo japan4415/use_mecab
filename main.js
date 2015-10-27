408f8a2b66cbf1b9a66b27fbc501a869be0e4cc6
discord4415 <raiden000discord@gmail.com>
2015-10-28 04:15:57 +0900

    呼び出しから表示

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ var usemecab = require('./usemecab.js');
 var url1 = "http://kenko100.jp/articles/121019001761/";
 
 kekka = function(url){
-  var ary = usemecab.chushutu(url);
+  var ary = usemecab.chushutu(url,function(ret){
+    console.log("retだと" + ret);
+  });
   console.log(ary);
 }
 
 kekka(url1);
-console.log("これから表示するよ\n" + usemecab.chushutu(url1));
