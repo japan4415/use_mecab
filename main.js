295c538d531e2209a101f4c681f95b13d8f221f2
discord4415 <raiden000discord@gmail.com>
2015-10-28 05:22:39 +0900

    呼び出しから表示

--- main.js
+++ main.js
@@ -38,5 +38,38 @@ usemecab2.js
 var json1 = require('./1st.json');
 var json2 = require('./2st.json');
 
-console.log(json1);
-console.log(json2);
+var kikyaku = 1;
+
+function keisan(hyosoi,hiritui){
+  this.hyoso = hyosoi;
+  this.hiritu = hiritui;
+}
+
+var json1_kikyaku[];
+var json2_kikyaku[];
+var ketugo[];
+
+json1.forEach(functon(line){
+  if(line.hiritu >= kikyaku){
+    json1_kikyaku.push(keisan(line.hyoso,line.hiritu));
+  }
+});
+
+json2.forEach(functon(line){
+  if(line.hiritu >= kikyaku){
+    json2_kikyaku.push(keisan(line.hyoso,line.hiritu));
+  }
+});
+
+json1_kikyaku.forEach(function(line){
+  json2_kikyaku.forEach(function(line2){
+    if(line.hyoso == line2.hyoso){
+      if(line.hiritu > line2.hiritu){
+        var kyotu = line.hiritu - (line.hiritu - line2.hiritu);
+      }
+      ketugo.push(line.hyoso,kyotu);
+    }
+  });
+});
+
+console.log(ketugo);
