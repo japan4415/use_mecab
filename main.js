233f6af019cb4d764bfa940632ad5bc4d45b30a0
discord4415 <raiden000discord@gmail.com>
2015-10-28 04:56:07 +0900

    呼び出しから表示

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
+/*
+非同期処理に悩まされたので取り敢えず分割。
+こっちはサスペンドで。
+
 var usemecab = require('./usemecab.js');
 
 var url1 = "http://kenko100.jp/articles/121019001761/";
 
 
-/*
+
 kekka = function(url){
   var ary = usemecab.chushutu(url);
   console.log(ary);
 }
-*/
+
 
 async.waterfall([
   function A(done){
@@ -20,3 +24,19 @@ async.waterfall([
 ],function(err){
   console.log('all comp');
 });
+
+*/
+
+
+/*
+こっからは
+usemecab1.js
+usemecab2.js
+を終わらせた後に使う関係性算出プログラムです。
+*/
+
+var json1 = require('./1st.json');
+var json2 = require('./2st.json');
+
+console.log(json1);
+console.log(json2);
